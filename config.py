2831852152e41edd609fa40ffa60e2e461503290
NoaDev <143912708+KekoOfficial@users.noreply.github.com>
2026-03-21 01:36:31 -0300

    Actualizar config.py

--- config.py
+++ config.py
@@ -1,11 +1,13 @@
 import os
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-DOWNLOAD_FOLDER = os.path.join(BASE_DIR, "downloads")
-LOG_FOLDER = os.path.join(BASE_DIR, "logs")
-
+# Carpeta temporal de descargas
+DOWNLOAD_FOLDER = os.path.join(os.getcwd(), "downloads")
 os.makedirs(DOWNLOAD_FOLDER, exist_ok=True)
+
+# Carpeta para logs
+LOG_FOLDER = os.path.join(os.getcwd(), "logs")
 os.makedirs(LOG_FOLDER, exist_ok=True)
 
-PORT = 5000
+# Carpeta de galería en Android
+GALLERY_FOLDER = "/storage/emulated/0/Movies/MallyCuts"
+os.makedirs(GALLERY_FOLDER, exist_ok=True)
