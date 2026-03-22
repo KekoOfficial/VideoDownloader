b205d7062d1d42a0028675e71090a7c0f9e73738
NoaDev <143912708+KekoOfficial@users.noreply.github.com>
2026-03-22 06:36:30 -0300

    Actualizar update_yt.py

--- scripts/update_yt.py
+++ scripts/update_yt.py
@@ -1,9 +1,9 @@
-import subprocess
-from utils.logger import log
+from subprocess import run
+from khassam import log
 
 def update_yt_dlp():
     log("🔄 Actualizando yt-dlp...")
-    result = subprocess.run(["pip", "install", "-U", "yt-dlp"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    result = run(["pip", "install", "-U", "yt-dlp"], capture_output=True)
     log(result.stdout.decode())
     if result.stderr:
         log(result.stderr.decode())
